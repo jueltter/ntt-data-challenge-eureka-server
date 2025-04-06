0f97da21f4f541660828a6713f55d75e228bc316
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-04-05 22:22:48 -0500

    initial commit

---
+++ src/test/java/ec/dev/samagua/ntt_data_challenge_eureka_server/NttDataChallengeEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.dev.samagua.ntt_data_challenge_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttDataChallengeEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
